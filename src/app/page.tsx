c3f952d1b43430035b217e753f8214c0cd84cdcf
Matt <solomatias321@gmail.com>
2025-02-08 14:17:20 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="max-w-7xl mx-auto px-4 mt-4 sm:px-6 lg:px-8">
       <div className="py-10">
-        <h1 className="text-4xl font-bold mb-4">Campaings:</h1>
+        <h1 className="text-4xl font-bold mb-4">Campaings:23mn</h1>
       </div>
     </main>
   );
